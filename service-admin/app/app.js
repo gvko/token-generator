d4598a8681cc5fe4cd5e65b8d8a4f5ab737a8b51
Galin Kostov <gako@esmiley.dk>
2018-09-16 18:24:55 +0200

    Lousy implementation of the token generation

--- service-admin/app/app.js
+++ service-admin/app/app.js
@@ -2,34 +2,39 @@
 
 const createError = require('http-errors');
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const jwtRouter = require('./routes/jwt');
+const tokensRouter = require('./routes/tokens');
 const usersRouter = require('./routes/users');
 
 const app = express();
 const port = process.env.PORT || '3000';
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(cors())
+  /*
+   * Routes
+   */
+  .use('/tokens', tokensRouter)
+  .use('/users', usersRouter)
 
-app.use('/jwt', jwtRouter);
-app.use('/users', usersRouter);
+  // catch 404 and forward to error handler
+  .use((req, res, next) => {
+    next(createError(404));
+  })
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+  // error handler
+  .use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 app.listen(port, () => {
   console.log(`Server started on port ${port} (container exposed: ${process.env.EXPOSED_PORT})`);
